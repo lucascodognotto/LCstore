1cad9ab86101e65316f2c45c78fb2c442ee08725
lucascodognotto <codognottolucas@gmail.com>
2022-10-24 19:16:11 -0300

    PRJ LCstore rev.16

---
+++ js/map.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-23.4865071,-47.4510347,17);
+        var mapOption = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.marker({
+            position: myLocation,
+            title: "Localização"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOptions);
+        marker.setMap(map);
+    }
+    init_map1();
+});
